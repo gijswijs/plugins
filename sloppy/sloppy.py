693834ada7963783f8d0f18a0b5a2852db197519
Gijs van Dam <gvandam@gmail.com>
2022-08-18 12:23:18 +0800

    First successful split payment
    Things to do:
    - Check for feature announcement before trying to split
    - Error handling in alternative route
    - Error handling in original route

--- sloppy/sloppy.py
+++ sloppy/sloppy.py
@@ -2,8 +2,10 @@
 # from math import ceil
 
 import threading
+import time
+import math
 
-from pyln.client import Plugin, RpcError
+from pyln.client import Plugin, RpcError, Millisatoshi
 
 from onion import OnionPayload, TlvPayload
 from primitives import ShortChannelId
@@ -32,8 +34,68 @@ def send_msg(peer_id, msgtype, contents):
     plugin.rpc.sendcustommsg(peer_id, msg.hex())
 
 
-def forward_payment(onion, pid, phash, request):
+def try_splitting(scid, chan, amt, peer, phash, request, payload):
+    # Exclude the channel given by the original route. We are trying to find an
+    # alternative route to the next peer to cover the amount in excess of the
+    # `spendable_msat` from that channel.
+    exclusions = [
+        "{scid}/{direction}".format(scid=scid, direction=chan['direction'])
+    ]
+
+    # FIXME: We should NOT be sending it here. We should only send it IF we have found an alternative route.
+    plugin.log("Relay original payment partially with full onion.")
+    request.set_result(
+        {"payload": payload, "result": "continue"})
+
+    # Try as many routes as possible before the timeout expires
+    # stop_time = int(time.time()) + plugin.try_timeout
+    # while int(time.time()) <= stop_time:
+    plugin.log("Search for alternative route")
+    route = get_alternative_route(amt, peer, exclusions)
+    # We exhausted all the possibilities, Game Over
+    if route is None:
+        plugin.log("No alternative round found.")
+        request.set_result({"result": "fail",
+                            "failure_message": "4002"})
+        return
+
+    plugin.log("Sending partial payment relay using payment_hash={}, route={}".format(
+        phash, route
+    ))
+    try:
+        plugin.rpc.sendpay(route, phash)
+        payment_result = plugin.rpc.waitsendpay(phash)
+        if (payment_result.get("status")
+                == "complete"):
+            plugin.log("Succesfully split payment relay for channel {},"
+                       "payment result {}".format(scid, payment_result))
+    except RpcError as e:
+        error = e.error['data']
+        plugin.log("Received error while finding alternative route {}"
+                   .format(error))
+        # The erring_channel field can not be present (shouldn't happen) or
+        # can be "0x0x0"
+        # FIXME: We shouldn't retry if this is because of the original payment failing.
+        erring_channel = error.get('erring_channel', '0x0x0')
+        if erring_channel != '0x0x0':
+            erring_direction = error['erring_direction']
+            exclusions.append("{}/{}".format(erring_channel,
+                                             erring_direction))
+            plugin.log("Excluding {} due to a failed attempt"
+                       .format(erring_channel))
+
+    # plugin.log("Timed out while trying to split a payment relay")
+    # request.set_result({"result": "continue"})
+    # plugin.log("Alternative route finding timed out.")
+    # request.set_result({"result": "fail",
+    #                     "failure_message": "4002"})
+
+
+def forward_payment(pid, phash):
     plugin.log("Forward payment with sendonion RPC call")
+
+    onion = plugin.splits[phash]['onion']
+
     fhop = {"id": pid,
             "amount_msat": onion['forward_amount'],
             "delay": onion['outgoing_cltv_value']}
@@ -43,15 +105,69 @@ def forward_payment(onion, pid, phash, request):
         if (rval.get("status") == "complete"):
             plugin.log(
                 "Succesfully forwarded payment with payment_hash {}".format(phash))
-            request.set_result({"result": "resolve",
-                                "payment_key": rval['payment_preimage']})
+            plugin.splits[phash]['main_payment'].set_result({"result": "resolve",
+                                                             "payment_key": rval['payment_preimage']})
+            for request in plugin.splits[phash]['additional_payments']:
+                request.set_result({"result": "resolve",
+                                    "payment_key": rval['payment_preimage']})
     except RpcError as e:
         error = e.error['data']
         plugin.log('Failed forwarded payment with error {}'.format(e))
         plugin.log('Error onion {}'.format(error['onionreply']))
-        request.set_result({"result": "fail",
-                            "failure_onion": error['onionreply']})
 
+        plugin.splits[phash]['main_payment'].set_result({"result": "fail",
+                                                         "failure_onion": error['onionreply']})
+        for request in plugin.splits[phash]['additional_payments']:
+            request.set_result({"result": "fail",
+                                "failure_message": "4002"})
+
+
+def collect_parts(onion, htlc, request):
+    plugin.log("Received a partial relayed payment")
+
+    phash = htlc['payment_hash']
+
+    amt = Millisatoshi(htlc['amount'])
+    total = plugin.splits[phash].get('total_received', 0)
+    total += amt.millisatoshis
+    plugin.splits[phash]['total_received'] = total
+
+    # # stop_time = int(time.time()) + 20
+    # # while int(time.time()) <= stop_time:
+    # # Does this part contain the relay information?
+    if 'short_channel_id' in onion:
+        # This is the original payment with the rest of the route in the onion.
+        # We will calculate the amount we want to receive based on the channel
+        # fee setting.
+
+        # Find the peer and channel that is next in path based on scid
+        peers = plugin.rpc.listpeers()['peers']
+        peer, chan = get_peer_and_channel(peers, onion['short_channel_id'])
+
+        # Calculate the fee we charge
+        fee = Millisatoshi(chan['fee_base_msat'])
+        # BOLT #7 requires fee >= fee_base_msat + ( amount_to_forward * fee_proportional_millionths / 1000000 )
+        famt = Millisatoshi(onion['forward_amount'])
+        fee += math.ceil((famt.millisatoshis *
+                            chan['fee_proportional_millionths']) // 10**6)
+
+        plugin.splits[phash]['total_expected'] = famt.millisatoshis + \
+            fee.millisatoshis
+        plugin.splits[phash]['onion'] = onion
+        plugin.splits[phash]['main_payment'] = request
+    else:
+        plugin.splits[phash]['additional_payments'].append(request)
+
+    plugin.log('Check if we received enough money {}'.format(
+        plugin.splits[phash]))
+    if ('total_expected' in plugin.splits[phash]) and (plugin.splits[phash]['total_received'] >= plugin.splits[phash]['total_expected']):
+        plugin.log("We have received enough money!")
+        peers = plugin.rpc.listpeers()['peers']
+        peer, _ = get_peer_and_channel(peers, plugin.splits[phash]['onion']['short_channel_id'])
+        t = threading.Thread(target=forward_payment, args=(
+            peer['id'], phash))
+        t.daemon = True
+        t.start()
 
 @plugin.async_hook('custommsg')
 def on_custommsg(peer_id, payload, plugin, request, **kwargs):
@@ -62,58 +178,65 @@ def on_custommsg(peer_id, payload, plugin, request, **kwargs):
     if mtype == MSGTYPE_SPLIT_PAYMENT:
         plugin.log("Received message about split payment to be received with payment hash {}".format(
             data.decode('ascii')))
-        plugin.splits.add(data.decode('ascii'))
+        # Create an empty set for collecting the partial payments, keyed by the payment hash.
+        plugin.splits[data.decode('ascii')] = {
+            "additional_payments": list()
+        }
 
     request.set_result({"result": "continue"})
+
+
+def get_alternative_route(amt, peer, exclusions):
+    """Find an alternative route to peer.
+
+    """
+
+    try:
+        route = plugin.rpc.getroute(
+            node_id=peer['id'],
+            msatoshi=amt,
+            riskfactor=1,
+            exclude=exclusions,
+        )['route']
+
+        return route
+    except RpcError:
+        plugin.log("Could not get a route, no remaining one? Exclusions : {}"
+                   .format(exclusions))
+        return None
 
 
 @plugin.async_hook("htlc_accepted")
 def on_htlc_accepted(htlc, onion, plugin, request, **kwargs):
     plugin.log("Got an incoming HTLC htlc={}, onion={}".format(htlc, onion))
 
-    payload = OnionPayload.from_hex(onion['payload'])
-    if not isinstance(payload, TlvPayload):
-        plugin.log("Payload is not a TLV payload")
-        request.set_result({"result": "continue"})
-
-    amt_to_forward = int.from_bytes(payload.get(2).value, 'big')
-    # The size of the payload is not allowed to change, so we have to use the
-    # same number of bytes for `amt_to_forward`
-    atflen = len(payload.get(2).value)
-    outgoing_cltv_value = payload.get(4)
-    short_channel_id = payload.get(6)
-
-    scid = ShortChannelId.from_bytes(short_channel_id.value)
-
-    plugin.log("Payload disected {}, {}, {}".format(amt_to_forward,
-               outgoing_cltv_value.value, short_channel_id.value))
-
-    # Find the peer based on scid
-    peers = plugin.rpc.listpeers()['peers']
-    peer, chan = get_peer_and_channel(peers, str(scid))
-    plugin.log("Found peer and channel {}, {}, for scid {}".format(
-        peer, chan, str(scid)))
-
-    adjusted_payload = TlvPayload()
-
-    if htlc['payment_hash'] in plugin.splits:
-        plugin.log("We expected this payment and will try a new payment with `sendonion` using the `next_onion` {} (We are Bob)".format(
-            onion['next_onion']))
-        t = threading.Thread(target=forward_payment, args=(
-            onion, peer['id'], htlc['payment_hash'], request))
-        t.daemon = True
-        t.start()
+    # The HTLC might be a split payment we expect to receive
+    split = plugin.splits.get(htlc['payment_hash'], None)
+    if split is not None:
+        plugin.log(
+            "We expected this payment and will collect all partial payments and try to relay with `sendonion` (We are Bob)")
+        # We will collect all parts of the split, relayed payment.
+        t1 = threading.Thread(target=collect_parts,
+                              args=(onion, htlc, request))
+        t1.daemon = True
+        t1.start()
         return
-    else:
-        plugin.log("We will NOT half the amount FOR NOW (We are Alice)")
-        #adjusted_amt = int(amt_to_forward / 2)
-        adjusted_amt = amt_to_forward
 
-    # adjusted_amt = amt_to_forward
+    # Check to see if the next channel has sufficient capacity
+    scid = onion['short_channel_id'] if 'short_channel_id' in onion else '0x0x0'
 
-    adjusted_payload.add_field(2, adjusted_amt.to_bytes(atflen, 'big'))
-    adjusted_payload.add_field(4, outgoing_cltv_value.value)
-    adjusted_payload.add_field(6, short_channel_id.value)
+    # Are we the destination? Then there's nothing to do. Continue.
+    # FIXME: We can be the destination of a split payment. We should handle that case.
+    if scid == '0x0x0':
+        request.set_result({"result": "continue"})
+        return
+
+    # Find the peer and channel that would be next in path based on scid
+    peers = plugin.rpc.listpeers()['peers']
+    peer, chan = get_peer_and_channel(peers, scid)
+    if peer is None or chan is None:
+        request.set_result({"result": "continue"})
+        return
 
     # If there isn't a peer or a channel there's no point in trying. Just let
     # the client handle and fail the htlc.
@@ -127,12 +250,63 @@ def on_htlc_accepted(htlc, onion, plugin, request, **kwargs):
         request.set_result({"result": "continue"})
         return
 
+    fwd_amt = Millisatoshi(onion['forward_amount'])
+
+    # If we have enough capacity just let it through now. Otherwise the
+    # Millisatoshi raises an error for negative amounts in the calculation
+    # below.
+    plugin.log("Capacity check: spendable_msat {}, forward_amount{}".format(
+        chan['spendable_msat'], fwd_amt))
+    if fwd_amt <= chan['spendable_msat']:
+        request.set_result({"result": "continue"})
+        return
+
+    payload = OnionPayload.from_hex(onion['payload'])
+    if not isinstance(payload, TlvPayload):
+        plugin.log("Payload is not a TLV payload.")
+        request.set_result({"result": "continue"})
+        return
+
+    # The size of the payload is not allowed to change, so we have to use the
+    # same number of bytes for `amt_to_forward`
+    falen = len(payload.get(2).value)
+
+    plugin.log("Payload disected {}, {}, {}".format(fwd_amt,
+               onion['outgoing_cltv_value'], scid))
+
+    adjusted_payload = TlvPayload()
+
+    plugin.log(
+        "We will forward 'spendable_msat' and complement it with additional payments (We are Alice)")
+
+    adjusted_amt = chan['spendable_msat']
+
+    if adjusted_amt == 0:
+        plugin.log("spendable_msat is 0, there is nothing to split.")
+        request.set_result({"result": "continue"})
+        return
+
+    plugin.log("We will forward 'spendable_msat' {} and complement it with additional payments (We are Alice)".format(
+        adjusted_amt.millisatoshis))
+
+    adjusted_payload.add_field(
+        2, adjusted_amt.millisatoshis.to_bytes(falen, 'big'))
+    adjusted_payload.add_field(4, payload.get(4).value)
+    adjusted_payload.add_field(6, payload.get(6).value)
+
     send_msg(peer['id'], MSGTYPE_SPLIT_PAYMENT, htlc['payment_hash'])
 
-    plugin.log("Adjusted payload: {}".format(adjusted_payload.to_hex()))
+    additional_amt = fwd_amt - chan['spendable_msat']
+    payload = adjusted_payload.to_hex()[2:]
 
-    request.set_result(
-        {"payload": adjusted_payload.to_hex()[2:], "result": "continue"})
+    t2 = threading.Thread(target=try_splitting, args=(
+        scid, chan, additional_amt, peer, htlc['payment_hash'], request, payload))
+    t2.daemon = True
+    t2.start()
+
+    # FIXME: We should only forward a partial payment if we know there is an alternative route.
+    # request.set_result(
+    #     {"payload": adjusted_payload.to_hex()[2:], "result": "continue"})
 
 
 @plugin.init()
@@ -140,8 +314,20 @@ def init(options, configuration, plugin: Plugin):
     plugin.log("sloppy.py initializing {}".format(configuration))
     plugin.node_id = plugin.rpc.getinfo()['id']
 
-    # Set of currently active split payments
-    plugin.splits = set()
+    plugin.try_timeout = int(options.get("sloppy-try-timeout"))
 
+    # Set of split payments to receive
+    plugin.splits = {}
+
+    # Set of split payments send by us
+    plugin.splits_sent = {}
+
+
+plugin.add_option(
+    "sloppy-try-timeout",
+    60,
+    "Number of seconds before we stop trying to find an alternative route to the next peer.",
+    opt_type="int"
+)
 
 plugin.run()
--- sloppy/test_sloppy.py
+++ sloppy/test_sloppy.py
@@ -4,6 +4,7 @@ import os
 import unittest
 import secrets
 import hashlib
+import time
 
 from pyln.testing.fixtures import *  # noqa: F401, F403
 from pyln.testing.utils import NodeFactory
@@ -56,7 +57,7 @@ def test_network(simple_network):
 def test_forward_payment_success(simple_network):
     """
     """
-    m, a, b, _, d = simple_network
+    m, a, b, c, d = simple_network
 
     # Dave creates an invoice that Mallory pays
 
@@ -71,12 +72,8 @@ def test_forward_payment_success(simple_network):
     m.rpc.pay(inv['bolt11'])
     m.wait_for_htlcs()
 
-    logging.info(m.rpc.listpays(inv['bolt11']))
     
-    assert m.rpc.listpays(inv['bolt11'])['pays'][0]['status'] == 'complete'
-
-    # Both Alice and Bob should have a message in their logs about an HTLC,
-    # since they both run the plugin
+    # Show messages in the log from the sloppy plugin
 
     r_a = re.compile('.*plugin-sloppy.py:*')
     filtered_a = list(filter(r_a.match, a.daemon.logs))
@@ -86,13 +83,25 @@ def test_forward_payment_success(simple_network):
     filtered_b = list(filter(r_b.match, b.daemon.logs))
     logging.info('log entries Bob: {}'.format("\n".join(filtered_b)))
 
-    assert(a.daemon.is_in_log(
-        r'Adjusted payload'
-    ))
+    assert m.rpc.listpays(inv['bolt11'])['pays'][0]['status'] == 'complete'
 
-    assert(b.daemon.is_in_log(
-        r'Received message about split payment'
-    ))
+    chan_m = m.rpc.listpeers(a.info['id'])['peers'][0]['channels'][0]
+    logging.info("Mallory forwarded {} to Alice.".format(chan_m['out_fulfilled_msat']))
+    assert Millisatoshi(chan_m['out_fulfilled_msat']) == 50002
+    chan_a1 = a.rpc.listpeers(b.info['id'])['peers'][0]['channels'][0]
+    logging.info("Alice forwarded {} to Bob.".format(chan_a1['out_fulfilled_msat']))
+    assert Millisatoshi(chan_a1['out_fulfilled_msat']) == 20000
+    chan_a2 = a.rpc.listpeers(c.info['id'])['peers'][0]['channels'][0]
+    logging.info("Alice forwarded {} to Charly.".format(chan_a2['out_fulfilled_msat']))
+    assert Millisatoshi(chan_a2['out_fulfilled_msat']) == 30002
+    chan_c = c.rpc.listpeers(b.info['id'])['peers'][0]['channels'][0]
+    logging.info("Charly forwarded {} to Bob.".format(chan_c['out_fulfilled_msat']))
+    assert Millisatoshi(chan_c['out_fulfilled_msat']) == 30001
+    chan_b = b.rpc.listpeers(d.info['id'])['peers'][0]['channels'][0]
+    logging.info("Bob forwarded {} to Dave.".format(chan_b['out_fulfilled_msat']))
+    assert Millisatoshi(chan_b['out_fulfilled_msat']) == 50000
+
+
 
 @unittest.skipIf(
     not DEVELOPER or DEPRECATED_APIS, "needs LIGHTNINGD_DEV_LOG_IO and new API"
@@ -134,13 +143,39 @@ def simple_network(node_factory: NodeFactory):
     opts = [{'alias': 'mallory'}, {'alias': 'alice', 'plugin': plugin}, {'alias': 'bob', 'plugin': plugin}, {'alias': 'charlie'}, {'alias': 'dave'}]
     m, a, b, c, d = node_factory.get_nodes(5, opts=opts)
 
-    capacity = Millisatoshi(10**9)
+    capacity = Millisatoshi(10**8)
 
-    # Open the channels
-    node_factory.join_nodes([m, a, b, d], fundamount=capacity.to_whole_satoshi(), wait_for_announce=True)
+    # Open the channels from M to A, B to D and B to A
+    node_factory.join_nodes([m, a], fundamount=capacity.to_whole_satoshi(), wait_for_announce=True)
+    node_factory.join_nodes([b, d], fundamount=capacity.to_whole_satoshi(), wait_for_announce=True)
+    node_factory.join_nodes([b, a], fundamount=capacity.to_whole_satoshi(), wait_for_announce=True)
+
+    chan_before = a.rpc.listpeers(b.info['id'])['peers'][0]['channels'][0]
+
+    # create an invoice so that Alice will have enough to fill the reserve on her side, plus a small fee allowance, but not enough for relaying 50000
+    amt = Millisatoshi(chan_before['our_reserve_msat']) + Millisatoshi(20000) 
+
+    inv = a.rpc.invoice(
+        amt.millisatoshis,
+        "fill", "fill"
+    )
+
+    # Wait for the invoice to settle(?)
+    time.sleep(1)
+
+    # We will drain the channel now, when there is only one route from A to B.
+    # Otherwise Route Randomization will kick in and make draining
+    # unpredictable.
+    logging.info("Alice has a channel with Bob, spendable_msatoshi before filling: {}".format(chan_before['spendable_msat']))
+    logging.info("Amount of draining invoice: {}".format(amt))
+    b.rpc.pay(inv['bolt11'])
+    b.wait_for_htlcs()
+
+    chan_after = a.rpc.listpeers(b.info['id'])['peers'][0]['channels'][0]
+    logging.info("Alice has a channel with Bob, spendable_msatoshi after filling: {}".format(chan_after['spendable_msat']))
+
+    # Open the channels from C to B and from A to C
     node_factory.join_nodes([c, b], fundamount=capacity.to_whole_satoshi(), wait_for_announce=True)
     node_factory.join_nodes([a, c], fundamount=capacity.to_whole_satoshi(), wait_for_announce=True)
-
-    
 
     return m, a, b, c, d
