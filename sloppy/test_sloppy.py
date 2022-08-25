29b61ddc527a77b157d5ab9d8708273a54ac03e9
Gijs van Dam <gvandam@gmail.com>
2022-08-25 10:51:15 +0800

    Add test cases
    - Split payments that need to be collected and not forwarded:
    test_forward_payment_success_to_Bob
    
    - Do a BDA on channel AB and get a wrong value:
    test_bda_channel_ab

--- sloppy/test_sloppy.py
+++ sloppy/test_sloppy.py
@@ -5,6 +5,7 @@ import os
 import secrets
 import time
 import unittest
+from random import random
 
 from pyln.testing.fixtures import *  # noqa: F401, F403
 from pyln.testing.utils import NodeFactory
@@ -14,6 +15,9 @@ from utils import (DEPRECATED_APIS, DEVELOPER, Millisatoshi, RpcError,
 
 currdir = os.path.dirname(__file__)
 plugin = os.path.join(currdir, 'sloppy.py')
+bda_plugin = os.path.join(os.path.dirname(currdir), 'bda/bda.py')
+getfixedroute_plugin = os.path.join(os.path.dirname(
+    currdir), 'getfixedroute/getfixedroute.py')
 
 
 @unittest.skipIf(
@@ -57,7 +61,7 @@ def test_network(simple_network):
 @unittest.skipIf(
     not DEVELOPER or DEPRECATED_APIS, "needs LIGHTNINGD_DEV_LOG_IO and new API"
 )
-def test_forward_payment_success(simple_network):
+def test_forward_payment_success_to_Dave(simple_network):
     """
     """
     m, a, b, c, d = simple_network
@@ -67,7 +71,7 @@ def test_forward_payment_success(simple_network):
     # Rebalance channel A-B so that it doesn't have zero funds in the A->B direction but not enough to relay the payment
     rebalance(a, b, (amt/5)*2)
 
-    # Rebalance channel B-C so that it doesn't have enough funds to relay the payment in the C->B direction, but enough combined with A->B. 
+    # Rebalance channel B-C so that it doesn't have enough funds to relay the payment in the C->B direction, but enough combined with A->B.
     rebalance(c, b, (amt/5)*3 + 1)
 
     # Rebalance channel D-B so that it does have enough funds to relay the payment in the B->D direction.
@@ -111,6 +115,55 @@ def test_forward_payment_success(simple_network):
         chan_b['out_fulfilled_msat']))
     assert Millisatoshi(chan_b['out_fulfilled_msat']) == 50000
 
+@unittest.skipIf(
+    not DEVELOPER or DEPRECATED_APIS, "needs LIGHTNINGD_DEV_LOG_IO and new API"
+)
+def test_forward_payment_success_to_Bob(simple_network):
+    """
+    """
+    m, a, b, c, d = simple_network
+
+    amt = Millisatoshi(50000)
+
+    # Rebalance channel A-B so that it doesn't have zero funds in the A->B direction but not enough to relay the payment
+    rebalance(a, b, (amt/5)*2)
+
+    # Rebalance channel B-C so that it doesn't have enough funds to relay the payment in the C->B direction, but enough combined with A->B.
+    rebalance(c, b, (amt/5)*3 + 1)
+
+    # Bob creates an invoice that Mallory pays
+
+    inv = b.rpc.invoice(
+        amt,
+        "test", "test"
+    )
+
+    m.rpc.pay(inv['bolt11'])
+    m.wait_for_htlcs()
+
+    # Show messages in the log from the sloppy plugin
+
+    show_logs(a, 'plugin-sloppy.py:')
+    show_logs(b, 'plugin-sloppy.py:')
+
+    assert m.rpc.listpays(inv['bolt11'])['pays'][0]['status'] == 'complete'
+
+    chan_m = m.rpc.listpeers(a.info['id'])['peers'][0]['channels'][0]
+    logging.info("Mallory forwarded {} to Alice.".format(
+        chan_m['out_fulfilled_msat']))
+    assert Millisatoshi(chan_m['out_fulfilled_msat']) == 50001
+    chan_a1 = a.rpc.listpeers(b.info['id'])['peers'][0]['channels'][0]
+    logging.info("Alice forwarded {} to Bob.".format(
+        chan_a1['out_fulfilled_msat']))
+    assert Millisatoshi(chan_a1['out_fulfilled_msat']) == 20000
+    chan_a2 = a.rpc.listpeers(c.info['id'])['peers'][0]['channels'][0]
+    logging.info("Alice forwarded {} to Charly.".format(
+        chan_a2['out_fulfilled_msat']))
+    assert Millisatoshi(chan_a2['out_fulfilled_msat']) == 30001
+    chan_c = c.rpc.listpeers(b.info['id'])['peers'][0]['channels'][0]
+    logging.info("Charly forwarded {} to Bob.".format(
+        chan_c['out_fulfilled_msat']))
+    assert Millisatoshi(chan_c['out_fulfilled_msat']) == 30000
 
 @unittest.skipIf(
     not DEVELOPER or DEPRECATED_APIS, "needs LIGHTNINGD_DEV_LOG_IO and new API"
@@ -141,7 +194,7 @@ def test_fail_alternative_route_at_CB(simple_network):
         amt,
         "test", "test"
     )
-    
+
     try:
         m.rpc.pay(inv['bolt11'])
     except RpcError as e:
@@ -151,6 +204,7 @@ def test_fail_alternative_route_at_CB(simple_network):
 
     show_logs(a, 'plugin-sloppy.py:')
     show_logs(b, 'plugin-sloppy.py:')
+
 
 @unittest.skipIf(
     not DEVELOPER or DEPRECATED_APIS, "needs LIGHTNINGD_DEV_LOG_IO and new API"
@@ -170,7 +224,7 @@ def test_fail_main_route_at_BD(simple_network):
     # Rebalance channel D-B so that it does have enough funds to relay the payment in the B->D direction.
     rebalance(b, d, amt*2)
 
-    # Rebalance channel B-C so that it doesn't have enough funds to relay the payment in the C->B direction, but enough combined with A->B. 
+    # Rebalance channel B-C so that it doesn't have enough funds to relay the payment in the C->B direction, but enough combined with A->B.
     rebalance(c, b, (amt/5)*3 + 1)
 
     # Create a random payment hash
@@ -181,9 +235,11 @@ def test_fail_main_route_at_BD(simple_network):
     chan = a.rpc.listpeers(c.info['id'])[
         'peers'][0]['channels'][0]
     exclusions = [
-        "{scid}/{direction}".format(scid=chan['short_channel_id'], direction=chan['direction'])
+        "{scid}/{direction}".format(scid=chan['short_channel_id'],
+                                    direction=chan['direction'])
     ]
-    route = m.rpc.getroute(d.info['id'], amt, riskfactor=0, exclude=exclusions)["route"]
+    route = m.rpc.getroute(
+        d.info['id'], amt, riskfactor=0, exclude=exclusions)["route"]
     try:
         m.rpc.sendpay(route, phash, msatoshi=amt)
         logging.info("WaitSendPay: {}".format(m.rpc.waitsendpay(phash)))
@@ -197,6 +253,32 @@ def test_fail_main_route_at_BD(simple_network):
     show_logs(a, 'plugin-sloppy.py:')
     show_logs(b, 'plugin-sloppy.py:')
 
+@unittest.skipIf(
+    not DEVELOPER or DEPRECATED_APIS, "needs LIGHTNINGD_DEV_LOG_IO and new API"
+)
+def test_bda_channel_ab(simple_network):
+    m, a, b, c, _ = simple_network
+
+    amt = Millisatoshi(50000)
+
+    # Rebalance channel A-B so that it doesn't have zero funds in the A->B direction but not enough to relay the payment
+    rebalance(a, b, (amt/5)*2)
+
+    # Rebalance channel B-C so that it's perfectly balanced
+    rebalance(c, b, (amt/5)*3 + 1)
+
+
+    left, right = m.rpc.bda(a.info["id"], b.info["id"])
+
+    chan_ab = a.rpc.listpeers(b.info['id'])['peers'][0]['channels'][0]
+    chan_cb = c.rpc.listpeers(b.info['id'])['peers'][0]['channels'][0]
+
+    assert left != chan_ab['to_us_msat']
+
+    total = Millisatoshi(chan_ab['our_reserve_msat'])+ Millisatoshi(chan_ab['spendable_msat'])+ Millisatoshi(chan_cb['spendable_msat'])
+
+    logging.info("The BDA returned a value of {}, which should not be the actual value of `to_us_msat` in that channel, which is {}".format(left, chan_ab['to_us_msat']))
+    logging.info("What the BDA actually found is the reserve of {} in channel A-B, and the combined `spendable_msat` of channel A-B and C-B: {}, {}. Totalling: {}.".format(chan_ab['our_reserve_msat'], chan_ab['spendable_msat'], chan_cb['spendable_msat'], total))
 
 @pytest.fixture
 def simple_network(node_factory: NodeFactory):
@@ -210,7 +292,7 @@ def simple_network(node_factory: NodeFactory):
            C
 
     """
-    opts = [{'alias': 'mallory'}, {'alias': 'alice', 'plugin': plugin}, {
+    opts = [{'alias': 'mallory', 'plugin': [getfixedroute_plugin, bda_plugin]}, {'alias': 'alice', 'plugin': plugin}, {
         'alias': 'bob', 'plugin': plugin}, {'alias': 'charlie'}, {'alias': 'dave'}]
     m, a, b, c, d = node_factory.get_nodes(5, opts=opts)
 
@@ -234,22 +316,21 @@ def rebalance(receiver, payer, amt: Millisatoshi):
         'peers'][0]['channels'][0]
 
     # Calculate unkept reserve
-    reserve_adj = max(Millisatoshi(
-        chan_before['our_reserve_msat']) - Millisatoshi(chan_before['to_us_msat']), Millisatoshi(0))
+    reserve_adj = Millisatoshi(max(Millisatoshi(chan_before['our_reserve_msat']).millisatoshis - Millisatoshi(chan_before['to_us_msat']).millisatoshis, 0))
 
     # create an invoice for the rebalancing amount, adjusted for possible reserve that is required to be kept.
     amt = reserve_adj + amt
 
     inv = receiver.rpc.invoice(
         amt,
-        "fill", "fill"
+        "rebalance-{}".format(random()), "rebalance"
     )
 
     # Wait for the invoice to settle(?)
     time.sleep(1)
 
     # We will rebalance the channel now. We don't need to exclude the alternative route B-C-A, since there is not enough funds C->A to relay. So we don't have to worry about route randomization.
-    logging.info("{} has a channel with {}, spendable_msatoshi before filling: {}".format(
+    logging.info("{} has a channel with {}, spendable_msat before filling: {}".format(
         alias_a, alias_b, chan_before['spendable_msat']))
     logging.info("Amount of draining invoice: {}".format(amt))
     payer.rpc.pay(inv['bolt11'])
@@ -257,7 +338,7 @@ def rebalance(receiver, payer, amt: Millisatoshi):
 
     chan_after = receiver.rpc.listpeers(payer.info['id'])[
         'peers'][0]['channels'][0]
-    logging.info("{} has a channel with {}, spendable_msatoshi after filling: {}".format(
+    logging.info("{} has a channel with {}, spendable_msat after filling: {}".format(
         alias_a, alias_b, chan_after['spendable_msat']))
 
 
